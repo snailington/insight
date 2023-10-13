331e9ef92a9f1c5fe227a599154b36dda9521ccd
snail <homeless@snail.moe>
2023-10-13 02:32:32 -0400

    added turn notification to StatusLine

--- src/StatusLine.tsx
+++ src/StatusLine.tsx
@@ -1,16 +1,32 @@
-import { Insight } from "./backend/Insight";
+import {useEffect, useState } from "react";
+import {getOwner, Insight} from "./backend/Insight";
 import "./StatusLine.css";
+import OBR from "@owlbear-rodeo/sdk";
 
 function StatusLine({insight} : {insight: Insight}) {
+    const [playerId, setPlayerId] = useState("");
     const content: JSX.Element[] = [];
     
+    useEffect(() => {
+        getOwner(insight.currentId).then(async (owner) => {
+            if(OBR.player.id == owner) {
+                await OBR.action.setBadgeText("!");
+            } else {
+                await OBR.action.setBadgeText(undefined);
+            }
+            setPlayerId(owner);
+        });
+    }, [insight, playerId]);
+
     if(insight.turn == 0) {
-        content.push(<span>Roll for initiative</span>);
+        content.push(<span key="blank">Roll for initiative</span>);
     } else {
-        content.push(<span className="turn">Turn {insight.turn}</span>);
+        content.push(<span key="turn" className="turn">Turn {insight.turn}</span>);
     }
-    
-    // todo: append alerts to indicate player's turn is next or current
+
+    if(playerId == OBR.player.id) {
+        content.push(<span key="yourturn" className="alert">Your Turn</span>)
+    }
     
     return (
         <div className="status">
